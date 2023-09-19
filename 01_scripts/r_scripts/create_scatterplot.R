5afa95264368c4aeb760cb73e88f7239ce4f3114
oboven <oboven@live.ca>
2023-09-19 09:20:06 -0700

    created a script to make a scatterplot of bill length vs depth, sorted by colour

---
+++ 01_scripts/r_scripts/create_scatterplot.R
@@ -0,0 +1,21 @@
+#This script plots bill length versus depth for the three species of the *palmerpenguins* dataset
+
+#install the below packages if they are not already installed 
+
+library(ggplot2)
+library(palmerpenguins)
+
+#create a dataframe indicating which colour each species should appear on the plot
+
+colours <- data.frame (species  = c("Adelie", "Chinstrap", "Gentoo"),
+                       color = c("darkorange", "blue", "cyan2")
+)
+
+#join this new dataframe to the penguins dataframe 
+
+penguins_col <- merge(x=penguins, y=colours, by="species", all.x=TRUE)
+  
+#create a simple scatter plot, with species distinguished by colour
+
+plot(penguins_col$bill_depth_mm, penguins_col$bill_length_mm, pch=16, col= penguins_col$color, xlab = "Bill length (mm)", ylab = "Bill depth (mm)")
+
